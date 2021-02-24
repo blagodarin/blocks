3ef5b5524b8b50866aa55178a2dc717bfeb2bcde
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-02-25 02:02:40 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// This file is part of the Blocks game.
+// Copyright (C) Sergei Blagodarin.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <yttrium/main.h>
+
+int ymain(int, char**)
+{
+	return 0;
+}
